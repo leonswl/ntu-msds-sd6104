c55d94317a34cde90b8e8cc4e35ba4fd47162a27
Eugene Ho <genehhz@gmail.com>
2025-04-20 22:15:26 +0800

    edit asso

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def create_arg_parser():
     parser.add_argument(
         "--columns_to_remove",
         type=str,
-        default="['Inspection ID', 'AKA Name', 'Latitude', 'Longitude', 'raw_violation', 'violation_comment', 'parse_error', 'error_reason', 'Facility Type', 'City', 'Inspection Type', 'City Cleaned', 'State']",
+        default=['Inspection ID', 'AKA Name', 'Facility Type', 'City', 'State', 'Inspection Type', 'Latitude', 'Longtitude', 'raw_violation', 'violation_comment', 'parse_error', 'error_reason', 'City_normalised', 'Address'],
         help="Stringified list of column names to remove",
     )
 
